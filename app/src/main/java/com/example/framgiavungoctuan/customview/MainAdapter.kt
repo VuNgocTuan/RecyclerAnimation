08fcd95f1ad061b4a6a59128a5472b8cd7d7d21a
Vu Ngoc Tuan <vungoctuan203@gmail.com>
2018-04-03 23:08:47 +0700

    Fix animation

--- app/src/main/java/com/example/framgiavungoctuan/customview/MainAdapter.kt
+++ app/src/main/java/com/example/framgiavungoctuan/customview/MainAdapter.kt
@@ -1,27 +1,20 @@
 package com.example.framgiavungoctuan.customview
 
-import android.support.v4.view.ViewCompat
-import android.support.v4.view.ViewPropertyAnimatorListener
+import android.animation.ObjectAnimator
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.AnticipateInterpolator
 import android.view.animation.AnticipateOvershootInterpolator
-import android.view.animation.BounceInterpolator
-import android.view.animation.TranslateAnimation
 import android.widget.ImageView
-import jp.wasabeef.recyclerview.animators.holder.AnimateViewHolder
 
 class MainAdapter : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
     private var mTexts = ArrayList<String>()
 
     init {
-        mTexts.add("1")
-        mTexts.add("2")
-        mTexts.add("3")
-        mTexts.add("4")
-        mTexts.add("5")
+        for (i in 1..50) {
+            mTexts.add("1")
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -35,42 +28,19 @@ class MainAdapter : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        if (mTexts[position] == "x") {
-            holder.image.setImageResource(R.drawable.manga_2)
-        }
+        holder.setData(mTexts[position])
+        startAnimation(holder.itemView, position)
     }
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view), AnimateViewHolder {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val image: ImageView = view.findViewById(R.id.image_item)
 
-        override fun preAnimateAddImpl(holder: RecyclerView.ViewHolder?) {
-            itemView.translationY = -itemView.height * 1.5f
+        fun setData(text: String) {
+            println("Text: " + text)
+            if (text == "x"){
+                image.setImageResource(R.drawable.manga_2)
+            }
         }
-
-        override fun preAnimateRemoveImpl(holder: RecyclerView.ViewHolder?) {
-            ViewCompat.setTranslationZ(itemView, -1f)
-        }
-
-        override fun animateAddImpl(holder: RecyclerView.ViewHolder?, listener: ViewPropertyAnimatorListener?) {
-            ViewCompat.animate(itemView)
-                    .translationY(0f)
-                    .setInterpolator(AnticipateOvershootInterpolator())
-                    .setDuration(500)
-                    .setListener(listener)
-                    .start()
-        }
-
-        override fun animateRemoveImpl(holder: RecyclerView.ViewHolder?, listener: ViewPropertyAnimatorListener?) {
-            ViewCompat.animate(itemView)
-                    .translationY(0f)
-                    .alpha(0f)
-                    .setInterpolator(AnticipateInterpolator())
-                    .setDuration(600)
-                    .setListener(listener)
-                    .start()
-        }
-
-
     }
 
     fun removeItem(position: Int) {
@@ -83,12 +53,12 @@ class MainAdapter : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
         notifyItemInserted(position)
     }
 
-    fun startAnimation(itemView: View) {
-        itemView.translationY = 50f
-        ViewCompat.animate(itemView)
-                .translationY(0f)
-                .setInterpolator(AnticipateOvershootInterpolator())
-                .setDuration(500)
-                .start()
+    private fun startAnimation(itemView: View, position: Int) {
+        val animator = ObjectAnimator.ofFloat(itemView, "translationY", 0f, 300 * (1 - position / 10.0f)
+                , 0f)
+        animator.interpolator = AnticipateOvershootInterpolator()
+        animator.startDelay = 100
+        animator.duration = 1000
+        animator.start()
     }
 }
